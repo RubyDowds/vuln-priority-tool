ecbc998013cfb9064405f8c9e3aa2f40c7f6e573
Ruby Dowds <rubydowds1@gmail.com>
2026-08-11 20:51:51 +1000

    splitting query collection for vuln and priority collections

--- backend/app/vector_store/chroma_client.py
+++ backend/app/vector_store/chroma_client.py
@@ -4,8 +4,14 @@ import chromadb
 
 client = chromadb.PersistentClient(path="./chroma_db") # persists to disk like your kev.db
 
-def get_collection():
+def get_vulnerabilities_collection():
     return client.get_or_create_collection(
         name="vulnerabilities",
+        metadata={"hnsw:space": "cosine"}  # cosine similarity for text embeddings
+    )
+
+def get_priorities_collection():
+    return client.get_or_create_collection(
+        name="priorities",
         metadata={"hnsw:space": "cosine"}  # cosine similarity for text embeddings
     )
